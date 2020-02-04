6c29f52b235be9f3a038282d6f23cfbdd337ddbe
UmbertoV <umberto.ottone@gmail.com>
2020-02-04 21:00:20 +0100

    Pagina ccreazione fatta

--- creazione.php
+++ creazione.php
@@ -9,12 +9,27 @@ include 'layout/head.php';
 
         </div>
         <div class="col-sm-6 text-right">
-            <a id="botton-right" class="btn btn-success" href="creazione.php"> Torna alla Home page </a>
+            <a id="botton-right" class="btn btn-success" href="index.php"> Torna alla Home page </a>
         </div>
     </div>
 
     <div class="row">
         <div class="col-sm-12">
+            <form method="post" action="insert.php">
+              <div class="form-group">
+                <label for="numero_stanza"> Numero Stanza </label>
+                <input name="numero_stanza" type="text" class="form-control" id="numero_stanza" placeholder="Numero stanza">
+              </div>
+              <div class="form-group">
+                  <label for="piano"> Piano </label>
+                  <input name="piano" type="text" class="form-control" id="piano" placeholder="Piano">
+              </div>
+              <div class="form-group">
+                  <label for="letti"> Numero Letti </label>
+                  <input name="numero_letti" type="text" class="form-control" id="letti" placeholder="Numero Letti">
+              </div>
+              <button type="submit" class="btn btn-primary"> Crea Stanza </button>
+            </form>
         </div>
     </div>
 </div>
--- function.php
+++ function.php
@@ -24,6 +24,23 @@ function esegui_query($query) {
     }
 }
 
+function controlla_dati_stanza($numero_stanza, $piano, $letti) {
+    if(
+        !empty($numero_stanza) &&
+        !empty($piano) &&
+        !empty($letti) &&
+        is_numeric($numero_stanza) &&
+        is_numeric($piano) &&
+        is_numeric($letti) &&
+        intval($numero_stanza) > 0 &&
+        intval($piano) > 0 &&
+        intval($letti) > 0
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 
